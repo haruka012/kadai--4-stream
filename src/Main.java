ab8c848596829947971c37a16dfa36069737708f
haruka012 <h.shino0828@gmail.com>
2023-09-03 14:27:01 +0900

    プロジェクトをセットアップ

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String,String> disneyMap = new HashMap<>();
+        disneyMap.put("Mickey","mouse");
+        disneyMap.put("Minnie","mouse");
+        disneyMap.put("Donald","duck");
+        disneyMap.put("Daisy","duck");
+        disneyMap.put("Goofy","dog");
+
+        Stream<Map.Entry<String, String>> stream = disneyMap.entrySet().stream()
+                .filter(entry -> entry.getValue().contentEquals("duck"));
+        System.out.println(Arrays.toString(stream.toArray()));
+
+    }
+}
---
+++ src/Main2.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Locale;
+
+public class Main2 {
+    public static void main(String[] args){
+        List<String> names = List.of("chip","dale","pluto");
+        List<String> sortedResult = names.stream().sorted().toList();
+        System.out.println(sortedResult);
+
+        List<String> result = names.stream().filter(name -> name.contains("c")).toList();
+        System.out.println(result);
+
+        List<String> result2 = names.stream().map(String::toUpperCase).toList();
+        System.out.println(result2);
+
+    }
+}
